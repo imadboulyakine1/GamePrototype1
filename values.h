ac1ab1ef627087437e18ac7069fbd7317a0148b2
siham <amirech.siham@etu.uae.ac.ma>
2024-10-23 07:02:38 -0400

    nothing

--- values.h
+++ values.h
@@ -1,10 +1,10 @@
 #define CONSTANTS_H
 
 //VideoScreen Values
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 1000;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
 const char* GAME_TITLE = "Maze Game";
 
 //Dificulty
-const int CELL_SIZE = 50;
+const int CELL_SIZE = 15;
 
