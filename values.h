92200277cfe3406c10f58b0f56a27f5972569b9a
imad <imad.abouu10@gmail.com>
2024-10-31 23:07:07 +0100

    yes

--- values.h
+++ values.h
@@ -6,9 +6,5 @@ const int SCREEN_HEIGHT = 500;
 const char* GAME_TITLE = "Maze Game";
 
 //Dificulty
-<<<<<<< HEAD
 const int CELL_SIZE = 9; //should be devided by 3 
-=======
-const int CELL_SIZE = 15;
->>>>>>> ac1ab1ef627087437e18ac7069fbd7317a0148b2
 
